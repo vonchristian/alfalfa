ecf52cdf4f04b2333acc20731fd0cf54e7023c49
Von Christian Halip <vc.halip@gmail.com>
2016-02-20 07:31:41 +0800

    Assets precompile for production.

--- app/assets/javascripts/equipment_cost.js
+++ app/assets/javascripts/equipment_cost.js
@@ -1,5 +1,5 @@
-function calculate() {
-    var myBox1 = document.getElementById('number_of_personnel').value;
+function calculate_equipment_cost() {
+    var myBox1 = document.getElementById('number_of_equipment').value;
     var myBox2 = document.getElementById('number_of_days').value;
     var myBox3 = document.getElementById('daily_rate').value;
     var result = document.getElementById('total_cost');
---
+++ app/assets/javascripts/labor_cost.js
@@ -0,0 +1,10 @@
+function calculate_labor_cost() {
+    var myBox1 = document.getElementById('number_of_personnel').value;
+    var myBox2 = document.getElementById('number_of_days').value;
+    var myBox3 = document.getElementById('daily_rate').value;
+    var result = document.getElementById('total_cost');
+    var myResult = myBox1 * myBox2 * myBox3;
+    result.value = myResult;
+
+
+  }
