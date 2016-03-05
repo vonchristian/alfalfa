dc17795f0f80028755f3d3d1a8b1c1a9017ebc6e
Von Christian Halip <vc.halip@gmail.com>
2016-03-06 02:02:53 +0800

    Deploy script.

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,7 +56,7 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      invoke :'puma:phased_restart'
+      invoke :'puma:start'
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
     end
