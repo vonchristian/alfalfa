fc3c9bc74c3d932f7d003c78185ce0c55e6a2545
Von Christian Halip <vc.halip@gmail.com>
2016-03-06 04:39:52 +0800

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,21 +2,46 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-require 'mina/puma'
+# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+
+# Basic settings:
+#   domain       - The hostname to SSH to.
+#   deploy_to    - Path to deploy into.
+#   repository   - Git repo to clone from. (needed by mina/git)
+#   branch       - Branch name to deploy. (needed by mina/git)
 
 set :domain, '192.168.254.102'
 set :deploy_to, '/var/www/alfalfa-construction'
-set :repository, 'https://github.com/vonchristian/alfalfa'
+set :repository, 'https://github.com/vonchristian/alfalfa.git'
 set :branch, 'master'
-set :user, 'deploy'
-set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
-set :forward_agent, true     # SSH forward_agent.
 set :term_mode, nil
+set :user, 'deploy'
+# For system-wide RVM install.
+#   set :rvm_path, '/usr/local/rvm/bin/rvm'
 
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+
+# Optional settings:
+#   set :user, 'foobar'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
+   set :forward_agent, true     # SSH forward_agent.
+
+# This task is the environment that is loaded for most commands, such as
+# `mina deploy` or `mina rake`.
 task :environment do
-   invoke :'rbenv:load'
+  # If you're using rbenv, use this to load the rbenv environment.
+  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
+ invoke :'rbenv:load'
+
+  # For those using RVM, use this to load an RVM version@gemset.
+  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
 end
 
+# Put any custom mkdir's in here for when `mina setup` is ran.
+# For Rails apps, we'll make some of the shared paths that are shared between
+# all releases.
 task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
