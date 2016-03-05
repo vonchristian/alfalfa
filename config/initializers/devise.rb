a97135e13abe8370c021e2d198bc625a83439195
Von Christian Halip <vc.halip@gmail.com>
2016-03-05 21:58:13 +0800

    fix typo error.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,6 @@ Devise.setup do |config|
   # by default. You can change it below and use your own secret key.
   config.secret_key = ENV['SECRET_KEY_BASE'] if Rails.env.production?
    # ==> Mailer Configuration
-  }
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
