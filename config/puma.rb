37e658d75c32bfbc3fc284008dcfcc9bfcd2bca3
Von Christian Halip <vc.halip@gmail.com>
2016-03-06 02:00:21 +0800

    Deploy script.

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env puma
+workers 4
 
 environment ENV['RAILS_ENV'] || 'production'
 
@@ -10,3 +11,10 @@ stdout_redirect "/var/www/alfalfa-construction/shared/tmp/log/stdout", "/var/www
 threads 0, 16
 
 bind "unix:///var/www/alfalfa-construction/shared/tmp/sockets/puma.sock"
+activate_control_app
+
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
