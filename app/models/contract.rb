bb5348b3b5a684d64127f8710de60a42f20edf7f
Christopher Pumihic <nnittop_909@live.com>
2016-03-27 14:46:48 +0800

    edit work detail labor cost.

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -4,6 +4,6 @@ class Contract < ActiveRecord::Base
   belongs_to :project
   belongs_to :contractor
   validates :contractor_id, presence: true, uniqueness: {scope: :project, message: "Already added"}
-  validates :amount_subcontracted, presence: true, numericality: {less_than: :remaining_uncontracted_amount }
+  validates :amount_subcontracted, presence: true, numericality: {less_than: :remaining_uncontracted_amount } unless :main_contractor?
   delegate :remaining_uncontracted_amount, to: :project
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -6,29 +6,27 @@ class Project < ActiveRecord::Base
 
   scope :joint_ventures, -> { where(type: 'JointVenture') }
 
-  before_save :create_contract
-  before_update :update_contract
+  has_many :collections
+  has_many :invoices, as: :invoiceable, foreign_key: 'invoiceable_id'
+  has_one    :notice_to_proceed, class_name: "ProjectDetails::NoticeToProceed", foreign_key: 'project_id'
+  belongs_to :main_contractor, class_name: "Contractor", foreign_key: 'main_contractor_id'
+  belongs_to :category
+  has_many  :employments
+  has_many :employees, through: :employments
+  has_many  :work_details
+  has_many :equipment_costs, through: :work_details
+  has_many :expenses, class_name: "Plutus::Entry", foreign_key: "commercial_document_id"
+  has_many :activities, class_name: "PublicActivity::Activity", foreign_key: "trackable_id"
+  has_many :contracts
+  has_many :contractors, through: :contracts
+  has_many :time_extensions, class_name: "ChangeOrders::TimeExtension"
+  has_many :amount_revisions, class_name: "ChangeOrders::AmountRevision"
+  has_many :work_accomplishments, through: :work_details
+  has_many :workers, class_name: "Employee"
+  has_many :remarks
 
-    has_many :collections
-    has_many :invoices, as: :invoiceable, foreign_key: 'invoiceable_id'
-    has_one    :notice_to_proceed, class_name: "ProjectDetails::NoticeToProceed", foreign_key: 'project_id'
-    belongs_to :main_contractor, class_name: "Contractor", foreign_key: 'main_contractor_id'
-    belongs_to :category
-    has_many  :employments
-    has_many :employees, through: :employments
-    has_many  :work_details
-    has_many :equipment_costs, through: :work_details
-    has_many :expenses, class_name: "Plutus::Entry", foreign_key: "commercial_document_id"
-    has_many :activities, class_name: "PublicActivity::Activity", foreign_key: "trackable_id"
-    has_many :contracts
-    has_many :contractors, through: :contracts
-    has_many :time_extensions, class_name: "ChangeOrders::TimeExtension"
-    has_many :amount_revisions, class_name: "ChangeOrders::AmountRevision"
-    has_many :work_accomplishments, through: :work_details
-    has_many :workers, class_name: "Employee"
-    has_many :remarks
-
-    validates :name, :cost, :implementing_office, :duration, :id_number, :address, presence: true
+  validates :name, :cost, :implementing_office, :duration, :id_number, :address, presence: true
+  
   def self.types
     ["JointVenture", "SubcontractWork"]
   end
@@ -36,12 +34,15 @@ class Project < ActiveRecord::Base
   def approved_change_orders
     self.work_details.map {|w| w.change_orders.addition.sum(:amount)}
   end
+
   def mobilization_fund
     self.cost * 0.15
   end
+
   def total_cost_of_work_details
     self.work_details.sum(:total_cost)
   end
+
   def previous_billings
     0
   end
@@ -50,18 +51,18 @@ class Project < ActiveRecord::Base
     time_extensions.blank? && amount_revisions.blank?
   end
 
-
-   def total_project_estimate
+  def total_project_estimate
     work_details.collect{|w| w.total_direct_cost}.sum
-   end
+  end
 
-    def remaining_uncontracted_amount
-      cost - contracts.sum(:amount_subcontracted)
-    end
+  def remaining_uncontracted_amount
+    cost - contracts.sum(:amount_subcontracted)
+  end
 
-   def total_collection
+  def total_collection
     self.collections.sum(:amount)
   end
+
   def remaining_collection
     revised_contract_amount -  total_collection
   end
@@ -75,11 +76,12 @@ class Project < ActiveRecord::Base
 
   def self.total_amount_revisions
     self.joins(:amount_revisions).sum(:amount)
-   end
+  end
 
   def self.total_cost_of_projects_with_revisions
     self.total_amount_revisions + self.sum(:cost)
   end
+
   def slippage
     if notice_to_proceed.present?
       (actual_accomplishment - target_accomplishment ).round(2)
@@ -87,6 +89,7 @@ class Project < ActiveRecord::Base
         0
     end
   end
+
   def latest_duration
     if time_extensions.present?
       time_extensions.sum(:number_of_days) + duration
@@ -95,35 +98,36 @@ class Project < ActiveRecord::Base
     end
   end
 
-    def actual_accomplishment
-      if self.work_accomplishments.present?
-        self.work_accomplishments.sum(:quantity)
-      else
-        0
-      end
-    end
-
-    def target_accomplishment
-      if notice_to_proceed.present?
-       ((days_elapsed / duration.to_f) * 100).round(2)
-     else
+  def actual_accomplishment
+    if self.work_accomplishments.present?
+      self.work_accomplishments.sum(:quantity)
+    else
       0
-     end
-end
+    end
+  end
+
+  def target_accomplishment
+    if notice_to_proceed.present?
+      ((days_elapsed / duration.to_f) * 100).round(2)
+    else
+      0
+    end
+  end
 
 
-    def no_amount_revisions?
-        amount_revisions.empty?
-    end
+  def no_amount_revisions?
+      amount_revisions.empty?
+  end
 
-    def revised_contract_amount
-        if no_amount_revisions?
-           cost
-        else
-        cost + amount_revisions.sum(:amount)
+  def revised_contract_amount
+    if no_amount_revisions?
+      cost
+    else
+      cost + amount_revisions.sum(:amount)
     end
-    end
-    def final_expiry_date
+  end
+
+  def final_expiry_date
     if  time_extensions.present?
        revised_expiry_date
     else
@@ -131,62 +135,60 @@ end
     end
   end
 
-    def start_date
-      if notice_to_proceed
+  def start_date
+    if notice_to_proceed
       notice_to_proceed.date
     end
+  end
+
+  def total_number_of_days_extended
+      self.time_extensions.sum(:number_of_days)
+  end
+
+  def days_elapsed
+    if self.notice_to_proceed.present?
+    ((Time.zone.now.to_i - start_date.to_i)/86400).floor
+  else
+    0
+  end
+  end
+
+  def remaining_days
+    revised_duration - days_elapsed
+  end
+
+
+  def expiry_date
+      if notice_to_proceed.present?
+      ((self.notice_to_proceed.date.to_date) + (self.duration))
+  end
+  end
+
+  def revised_expiry_date
+   if notice_to_proceed.present? && time_extensions.present?
+      self.notice_to_proceed.date + self.revised_duration.days
     end
+  end
 
-    def total_number_of_days_extended
-        self.time_extensions.sum(:number_of_days)
-    end
+  def revised_duration
+    self.duration + self.total_number_of_days_extended
+  end
 
-    def days_elapsed
-      if self.notice_to_proceed.present?
-      ((Time.zone.now.to_i - start_date.to_i)/86400).floor
-    else
-      0
-    end
-    end
+  def add_main_contractor_to_contractors
+    Contract.create(contractor_id: self.main_contractor.id, project_id: self.id ) if self.new_record?
+  end
 
-    def remaining_days
-      revised_duration - days_elapsed
-    end
+  def retention_amount
+    self.cost * 0.10
+  end
 
+  def trade_amount
+    self.cost - self.retention_amount
+  end
 
-    def expiry_date
-        if notice_to_proceed.present?
-        ((self.notice_to_proceed.date.to_date) + (self.duration))
-    end
-    end
-
-    def revised_expiry_date
-     if notice_to_proceed.present? && time_extensions.present?
-        self.notice_to_proceed.date + self.revised_duration.days
-      end
-    end
-
-    def revised_duration
-      self.duration + self.total_number_of_days_extended
-    end
-
-
-
-    def add_main_contractor_to_contractors
-      Contract.create(contractor_id: self.main_contractor.id, project_id: self.id ) if self.new_record?
-    end
-
-    def retention_amount
-      self.cost * 0.10
-    end
-
-    def trade_amount
-      self.cost - self.retention_amount
-    end
-
-     def add_to_accounts
-       Entry.create!(description: self.name, debit_amounts_attributes:[{amount: (self.trade_amount), account: "Accounts Receivable-Trade"},{amount: (self.retention_amount), account: "Accounts Receivable-Retention"}],
-                         credit_amounts_attributes:[amount: (self.cost), account: "Cost of Contracts"])
+   def add_to_accounts
+     Entry.create!(description: self.name, debit_amounts_attributes:[{amount: (self.trade_amount), account: "Accounts Receivable-Trade"},{amount: (self.retention_amount), account: "Accounts Receivable-Retention"}],
+                       credit_amounts_attributes:[amount: (self.cost), account: "Cost of Contracts"])
    end
 
    def update_work_detail_accomplishments_status
@@ -198,11 +200,11 @@ end
   end
 
   def create_contract
-    Contract.create(contractor_id: self.main_contractor.id, project_id: self.id) if self.new_record?
+    Contract.create!(contractor_id: self.main_contractor.id, project_id: self.id, amount_subcontracted: nil)
   end
 
   def update_contract
-    Contract.create(contractor_id: self.main_contractor.id, project_id: self.id)
+    Contract.update(contractor_id: self.main_contractor.id, project_id: self.id, amount_subcontracted: nil)
   end
 
 end
