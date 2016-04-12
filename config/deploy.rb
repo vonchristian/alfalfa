9ac83b89968ceae73ec648a8350cceca87d5381a
Christopher Pumihic <nnittop_909@live.com>
2016-04-12 22:47:29 +0800

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,8 +45,8 @@ end
 # For Rails apps, we'll make some of the shared paths that are shared between
 # all releases.
 task :setup => :environment do
-  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/log"]
-  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/log"]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/tmp/log"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/tmp/log"]
@@ -98,28 +98,27 @@ task :deploy => :environment do
     invoke :'deploy:cleanup'
 
     to :launch do
-      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
-      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+      invoke :'puma:restart'
     end
   end
 end
 
-# namespace :puma do
-#   desc "Start the application"
-#   task :start do
-#     queue 'echo "-----> Start Puma"'
-#     queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh start"
-#   end
+namespace :puma do
+  desc "Start the application"
+  task :start do
+    queue 'echo "-----> Start Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh start"
+  end
 
-#   desc "Stop the application"
-#   task :stop do
-#     queue 'echo "-----> Stop Puma"'
-#     queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh stop"
-#   end
+  desc "Stop the application"
+  task :stop do
+    queue 'echo "-----> Stop Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh stop"
+  end
 
-#   desc "Restart the application"
-#   task :restart do
-#     queue 'echo "-----> Restart Puma"'
-#     queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh restart"
-#   end
-# end
+  desc "Restart the application"
+  task :restart do
+    queue 'echo "-----> Restart Puma"'
+    queue "cd #{app_path} && RAILS_ENV=#{stage} && bin/puma.sh restart"
+  end
+end
