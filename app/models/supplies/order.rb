2836bfaeff77255f2530d5da08534a79a87cccc9
Von Christian Halip <vc.halip@gmail.com>
2016-05-06 09:58:12 +0800

    Edit order.

--- app/models/supplies/order.rb
+++ app/models/supplies/order.rb
@@ -4,7 +4,7 @@ class Supplies::Order < ActiveRecord::Base
   enum payment_status:[:paid, :unpaid]
   has_many :line_items, dependent: :destroy
 
-  accepts_nested_attributes_for :entry
+  
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
