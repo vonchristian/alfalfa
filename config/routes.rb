41148051571e10086e4a3a11b849bb0b1057f473
Christopher Pumihic <nnittop_909@live.com>
2016-05-19 15:11:33 +0800

    Edit monitoring sidebar page.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users", sessions: "users/sessions" }
 
 unauthenticated :user do
-  root :to => 'projects#index', :constraints => lambda { |request| request.env['warden'].user.nil? }, as: :unauthenticated_root
+  root :to => 'monitoring/projects#index', :constraints => lambda { |request| request.env['warden'].user.nil? }, as: :unauthenticated_root
 end
 
   root :to => 'monitoring/projects#index', :constraints => lambda { |request| request.env['warden'].user.role == 'monitoring_officer' if request.env['warden'].user }, as: :monitoring_root
