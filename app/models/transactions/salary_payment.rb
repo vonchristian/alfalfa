ffb50e03d7251d0e60e7ef79dfe4c02e768a986a
Von Christian Halip <vc.halip@gmail.com>
2016-05-25 13:26:05 +0800

    Searching feature on index page.

--- app/models/transactions/salary_payment.rb
+++ app/models/transactions/salary_payment.rb
@@ -1,5 +1,7 @@
-class Transactions::SalaryPayment < Accounting::Entry
-  def set_cash_advances_to_paid
-    self.entriable.set_cash_advances_to_paid
+module Transactions
+  class SalaryPayment < Accounting::Entry
+    def set_cash_advances_to_paid
+      self.entriable.set_cash_advances_to_paid
+    end
   end
 end
