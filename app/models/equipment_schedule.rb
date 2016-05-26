6a38c762e11c04b1d419cf29f7669062869a63fb
Christopher Pumihic <nnittop_909@live.com>
2016-05-26 17:42:01 +0800

    Fix Equipment Dashboard

--- app/models/equipment_schedule.rb
+++ app/models/equipment_schedule.rb
@@ -3,4 +3,6 @@ class EquipmentSchedule < ActiveRecord::Base
   belongs_to :project
   belongs_to :operator, class_name: 'Employee', foreign_key: "employee_id"
   delegate :full_name, to: :employee
+
+  validates_presence_of :start_date, :end_date, :project_id, :employee_id, :purpose
 end
