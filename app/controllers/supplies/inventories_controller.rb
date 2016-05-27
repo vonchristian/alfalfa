555fb28b80a0596d21f74fcc2ae476085dc964e1
Von Christian Halip <vc.halip@gmail.com>
2016-05-27 22:51:12 +0800

    Add policies for inventories.

--- app/controllers/supplies/inventories_controller.rb
+++ app/controllers/supplies/inventories_controller.rb
@@ -3,15 +3,18 @@ module Supplies
     layout 'supplies'
     def index
       @inventories = Supplies::Inventory.all.order('created_at DESC').page(params[:page]).per(10)
+      authorize @inventories, :index?
     end
 
     def new
       @inventory = Supplies::Inventory.new
+      authorize @inventory
       @inventory.stocks.build
     end
 
     def create
       @inventory = Supplies::Inventory.create(inventory_params)
+      authorize @inventory
       if @inventory.save
         # @inventory.update_accounts
         @inventory.create_activity :create, owner: current_user
@@ -23,14 +26,17 @@ module Supplies
 
     def show
       @inventory = Supplies::Inventory.find(params[:id])
+      authorize @inventory
     end
 
     def edit
       @inventory = Supplies::Inventory.find(params[:id])
+      authorize @inventory
     end
 
     def update
       @inventory = Supplies::Inventory.find(params[:id])
+      authorize @inventory
       if @inventory.update(inventory_params)
         redirect_to supplies_inventories_url, notice: 'Inventory updated successfully.'
       else
---
+++ app/policies/supplies/inventory_policy.rb
@@ -0,0 +1,48 @@
+module Supplies
+  class InventoryPolicy < ApplicationPolicy
+    attr_reader :current_user, :inventory
+
+    def initialize(user, inventory)
+      @user = user
+      @inventory = inventory
+    end
+
+    def index?
+      create?
+    end
+
+    def show?
+     create?
+    end
+
+    def create?
+    user.supply_officer?
+    end
+
+    def new?
+      create?
+    end
+
+    def edit?
+      user.supply_officer?
+    end
+
+    def update?
+      edit?
+    end
+
+    def destroy?
+      false
+    end
+
+    class Scope
+      def initialize(user, scope)
+        @user = user
+        @scope = scope
+      end
+      def resolve
+        scope
+      end
+    end
+  end
+end
