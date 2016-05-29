12f855dbe088673c70e9d8e64484f9c803493245
Von Christian Halip <vc.halip@gmail.com>
2016-05-29 16:23:08 +0800

    Edit project policy.

--- spec/policies/project_policy_spec.rb
+++ spec/policies/project_policy_spec.rb
@@ -9,65 +9,63 @@ describe ProjectPolicy do
 
    context "for a project engineer" do
     let(:user) { create(:user, role: :project_engineer) }
-     it { should permit(:show)    }
-    it { should permit(:create)  }
-    it { should permit(:new)     }
-    it { should permit(:update)  }
-    it { should permit(:edit)    }
-    it { should_not permit(:destroy) }
+     it { is_expected.to permit(:show)    }
+    it { is_expected.to permit(:create)  }
+    it { is_expected.to permit(:new)     }
+    it { is_expected.to permit(:update)  }
+    it { is_expected.to permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
     context "for an owner of the business " do
     let(:user) { create(:user, role: :owner) }
-     it { should permit(:show)    }
-    it { should_not permit(:create)  }
-    it { should_not permit(:new)     }
-    it { should_not  permit(:update)  }
-    it { should_not permit(:edit)    }
-    it { should_not permit(:destroy) }
+     it { is_expected.to_not permit(:show)    }
+    it { is_expected.to_not permit(:create)  }
+    it { is_expected.to_not permit(:new)     }
+    it { is_expected.to_not  permit(:update)  }
+    it { is_expected.to_not permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
   context "for a system administrator " do
     let(:user) { create(:user, role: :system_administrator) }
-     it { should permit(:show)    }
-    it { should_not permit(:create)  }
-    it { should_not permit(:new)     }
-    it { should_not  permit(:update)  }
-    it { should_not permit(:edit)    }
-    it { should_not permit(:destroy) }
+     it { is_expected.to_not permit(:show)    }
+    it { is_expected.to_not permit(:create)  }
+    it { is_expected.to_not permit(:new)     }
+    it { is_expected.to_not  permit(:update)  }
+    it { is_expected.to_not permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
   context "for a supply officer " do
     let(:user) { create(:user, role: :supply_officer) }
-     it { should permit(:show)    }
-    it { should_not permit(:create)  }
-    it { should_not permit(:new)     }
-    it { should_not  permit(:update)  }
-    it { should_not permit(:edit)    }
-    it { should_not permit(:destroy) }
+     it { is_expected.to_not permit(:show)    }
+    it { is_expected.to_not permit(:create)  }
+    it { is_expected.to_not permit(:new)     }
+    it { is_expected.to_not  permit(:update)  }
+    it { is_expected.to_not permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
   context "for an accounting officer " do
     let(:user) { create(:user, role: :accounting_officer) }
-     it { should permit(:show)    }
-    it { should_not permit(:create)  }
-    it { should_not permit(:new)     }
-    it { should_not permit(:update)  }
-    it { should_not permit(:edit)    }
-    it { should_not permit(:destroy) }
+    it { is_expected.to_not permit(:show)    }
+    it { is_expected.to_not permit(:create)  }
+    it { is_expected.to_not permit(:new)     }
+    it { is_expected.to_not permit(:update)  }
+    it { is_expected.to_not permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
   context "for a monitoring officer " do
     let(:user) { create(:user, role: :monitoring_officer) }
-     it { should permit(:show)    }
-    it { should permit(:create)  }
-    it { should permit(:new)     }
-    it { should  permit(:update)  }
-    it { should permit(:edit)    }
-    it { should_not permit(:destroy) }
+    it { is_expected.to permit(:show)    }
+    it { is_expected.to permit(:create)  }
+    it { is_expected.to permit(:new)     }
+    it { is_expected.to  permit(:update)  }
+    it { is_expected.to permit(:edit)    }
+    it { is_expected.to_not permit(:destroy) }
   end
 
 
 end
-
-
