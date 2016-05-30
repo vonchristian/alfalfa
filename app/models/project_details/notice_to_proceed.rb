e672fe1dc189a26d36155a08ce0ce0294ddd0a75
Von Christian Halip <vc.halip@gmail.com>
2016-05-30 13:25:15 +0800

    Validation notice to proceed.

--- app/models/project_details/notice_to_proceed.rb
+++ app/models/project_details/notice_to_proceed.rb
@@ -2,7 +2,7 @@ class ProjectDetails::NoticeToProceed < ActiveRecord::Base
   include PublicActivity::Common
   belongs_to :project
   validates :date, presence: true
-  validates_datetime :date, on_or_after: :project_created_at, on_or_after_message: "Must be after the date project was created"
+  # validates_datetime :date, on_or_after: :project_created_at, on_or_after_message: "Must be after the date project was created"
 
   delegate :created_at, to: :project, prefix: true
 end
