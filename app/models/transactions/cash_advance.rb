3b7289e4cc0bca4035348fd6193ceac7b693cef5
Von Christian Halip <vc.halip@gmail.com>
2016-06-07 13:11:29 +0800

    Edit time in cash advance filter.

--- app/models/transactions/cash_advance.rb
+++ app/models/transactions/cash_advance.rb
@@ -1,7 +1,4 @@
 class Transactions::CashAdvance < Accounting::Entry
-  def status
-    'Paid'
-  end
   def self.unpaid_amount_for(employee)
     (Asset.find_by_name( "Advances to Employees").credit_entries.where(entriable: employee).sum(:amount) - Asset.find_by_name( "Advances to Employees").debit_entries.where(entriable: employee).sum(:amount)).abs
   end
