5bffd04281fb36979d8204bb62e197719f99e61e
Von Christian Halip <vc.halip@gmail.com>
2016-06-07 15:43:42 +0800

    Edit fund transfer

--- app/controllers/accounting/petty_cash/fund_transfers_controller.rb
+++ app/controllers/accounting/petty_cash/fund_transfers_controller.rb
@@ -3,7 +3,7 @@ module Accounting
     layout "accounting"
     def new
       @petty_cash = Asset.find_by_name!("Petty Cash")
-      @entry = Transactions::FundTransfer.new
+      @entry = Accounting::Entry.new
       # authorize @entry
       @entry.credit_amounts.build
       @entry.debit_amounts.build
@@ -11,7 +11,7 @@ module Accounting
 
     def create
       @petty_cash = Asset.find_by_name!("Petty Cash")
-      @entry = Transactions::FundTransfer.new(entry_params)
+      @entry = Accounting::Entry.new(entry_params)
       # authorize @entry
       if @entry.save
         redirect_to accounting_petty_cash_index_path, notice: "Fund Transfer saved successfully."
@@ -26,7 +26,7 @@ module Accounting
 
     private
     def entry_params
-      params.require(:transactions_fund_transfer).permit(:date, :entriable_id, :entriable_type, :reference_number, :description, :credit_amounts_attributes=> [:amount, :account], :debit_amounts_attributes=> [:amount, :account])
+      params.require(:accounting_entry).permit(:date, :entriable_id, :entriable_type, :reference_number, :description, :credit_amounts_attributes=> [:amount, :account], :debit_amounts_attributes=> [:amount, :account])
     end
   end
 end
