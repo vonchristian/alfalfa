8cd3d85be4888bbfe7432c0ce028c8ef28550a89
Christopher Pumihic <nnittop_909@live.com>
2016-06-10 00:12:48 +0800

    Fix GUI

--- app/assets/javascripts/supplies/application.js
+++ app/assets/javascripts/supplies/application.js
@@ -20,6 +20,7 @@
 //= require supplies/admin
 //= require supplies/dynamic_select
 //= require labor_cost
+//= require purchase_order
 //= require supplies/bootstrap-checkbox-radio
 //= require supplies/entries
 //= require supplies/equipment_cost
