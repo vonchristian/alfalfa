3e6e88ffada33e34f2c19f72ad9d793b1ef042d5
Christopher Pumihic <nnittop_909@live.com>
2016-06-15 22:56:56 +0800

    Fix notice to proceed.

--- app/controllers/monitoring/notice_to_proceeds_controller.rb
+++ app/controllers/monitoring/notice_to_proceeds_controller.rb
@@ -8,15 +8,20 @@ module Monitoring
       authorize @notice_to_proceed
     end
 
-    def edit
-      @project = Project.find(params[:project_id])
-      @notice_to_proceed = ProjectDetails::NoticeToProceed.find(params[:id])
-      authorize @notice_to_proceed
-    end
-
     def create
       @project = Project.find(params[:project_id])
       @notice_to_proceed = @project.create_notice_to_proceed(notice_to_proceed_params)
+      authorize @notice_to_proceed
+      if @notice_to_proceed.save
+        redirect_to monitoring_project_path(@project), notice: "NTP saved sucessfully."
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @project = Project.find(params[:project_id])
+      @notice_to_proceed = ProjectDetails::NoticeToProceed.find(params[:id])
       authorize @notice_to_proceed
     end
 
@@ -25,6 +30,11 @@ module Monitoring
       @notice_to_proceed = ProjectDetails::NoticeToProceed.find(params[:id])
       authorize @notice_to_proceed
       @notice_to_proceed.update(notice_to_proceed_params)
+      if @notice_to_proceed.save
+        redirect_to monitoring_project_path(@project), notice: "NTP updated sucessfully."
+      else
+        render :edit
+      end
     end
 
     private
