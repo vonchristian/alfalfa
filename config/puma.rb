b0f2c3fee4f2427abebf7a792a34c271fe0e3c47
Von Christian Halip <vc.halip@gmail.com>
2016-06-23 10:30:38 +0800

    Edit typo"

--- config/puma.rb
+++ config/puma.rb
@@ -8,5 +8,5 @@ pidfile "/var/www/alfalfa-construction/shared/tmp/pids/puma.pid"
 stdout_redirect "/var/www/alfalfa-construction/shared/tmp/log/stdout", "/var/www/alfalfa-construction/shared/tmp/log/stderr"
 
 threads 0, 16
-r
+
 bind "unix:///var/www/alfalfa-construction/shared/tmp/sockets/puma.sock"
