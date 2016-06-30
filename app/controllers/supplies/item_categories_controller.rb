13babc39fe7ae6b4935ae41b50ced7435236cdd8
Christopher Pumihic <nnittop_909@live.com>
2016-06-30 13:55:37 +0800

    Fix Item Categories

--- app/controllers/supplies/item_categories_controller.rb
+++ app/controllers/supplies/item_categories_controller.rb
@@ -6,11 +6,11 @@ module Supplies
     end
 
     def create
-      @item_category = ItemCategory.new(item_category_params)
+      @item_category = ItemCategory.create(item_category_params)
     end
     private
     def item_category_params
-      params.require(:accounting_item_category).permit(:description, :item_category_type,  :date, :reference_number, :entriable_id, :entriable_type, :credit_amounts_attributes=> [:id, :amount, :account],:debit_amounts_attributes=> [:id, :amount, :account])
+      params.require(:item_category).permit(:name)
     end
   end
 end
