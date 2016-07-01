4725ea0d90b5e204f26315093954347febcd6c1b
Von Christian Halip <vc.halip@gmail.com>
2016-07-01 10:24:12 +0800

    Add employee cash advances.

--- app/controllers/accounting/transactions/cash_advances_controller.rb
+++ app/controllers/accounting/transactions/cash_advances_controller.rb
@@ -2,7 +2,7 @@ module Accounting
   module Transactions
     class CashAdvancesController < ApplicationController
       def scope_to_date
-        @entries = Accounting::Entry.cash_advance.created_between(params[:from_date], params[:to_date]).page(params[:page]).per(50)
+        @entries = Accounting::Entry.cash_advance.created_between(params[:from_date], params[:to_date])
         @from_date = params[:from_date] ? Time.parse(params[:from_date]) : Time.zone.now.beginning_of_day
         @to_date = params[:to_date] ? Time.parse(params[:to_date]) : Time.zone.now.end_of_day
         respond_to do |format|
